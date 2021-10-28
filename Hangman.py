125d03736b0282d21035f17c45d12ba98f802658
yunus <yunusemre4634@gmail.com>
2021-10-28 18:07:16 +0300

    Initial commit

---
+++ Hangman.py
@@ -0,0 +1,9 @@
+word_list = ["ardvark", "baboon", "camel"]
+import random
+chosen_word = random.choice(word_list)
+guess = input("Guess a letter").lower()
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
